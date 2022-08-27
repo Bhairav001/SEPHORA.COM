52bf3507b5e898464d3a54261b432927ce5dd7a9
Monojit Mondal <mondalmonojit1696@gmail.com>
2022-08-27 20:58:52 +0530

    file updated

---
+++ Navbar/index.js
@@ -0,0 +1,16 @@
+let div = document.getElementById('M-D-1');
+let p = document.getElementById('M-store-span');
+
+function downList(){
+    p.classList.toggle("open");
+    div.classList.toggle("open");
+}
+
+let div2 = document.getElementById('M-C-D');
+let p2 = document.getElementById('M-C-span');
+
+function drpDwn(){
+    p2.classList.toggle("open");
+    div2.classList.toggle("open");
+}
+
